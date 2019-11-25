b804f33350b5b02420928f90f7aed3cd3e3a71f1
Simon Walker <s.r.walker101@googlemail.com>
2019-11-25 21:53:04 +0000

    Update to include notify crates

--- src/main.rs
+++ src/main.rs
@@ -1,88 +1,9 @@
-use std::fs::{File, Metadata};
-use std::io::{BufRead, BufReader};
-use std::path::Path;
+use notify::{raw_watcher, RawEvent, RecursiveMode, Watcher};
 use std::sync::mpsc;
-use std::thread;
 use structopt::StructOpt;
 use tracing::{debug, info, span, warn, Level};
 
 type Result<T> = std::result::Result<T, Box<dyn std::error::Error>>;
-
-#[derive(Debug)]
-struct LogMessage {
-    raw_line: String,
-}
-
-struct LogFileTailer<'a> {
-    sender: &'a mpsc::Sender<LogMessage>,
-    reader: BufReader<File>,
-    // Used for logging
-    span: tracing::Span,
-    metadata: Metadata,
-}
-
-impl<'a> LogFileTailer<'a> {
-    fn new<P>(path: P, sender: &'a mpsc::Sender<LogMessage>) -> Result<Self>
-    where
-        P: AsRef<Path> + std::fmt::Debug,
-    {
-        let f = File::open(&path)?;
-        let metadata = f.metadata()?;
-        let reader = BufReader::new(f);
-        let span = span!(Level::INFO, "LogFileTailer", ?path);
-        Ok(Self {
-            sender,
-            metadata,
-            reader,
-            span,
-        })
-    }
-
-    fn run(&mut self) -> Result<thread::JoinHandle<()>> {
-        Ok(thread::spawn(move || {
-            self.start().unwrap();
-        }))
-    }
-
-    fn start(&mut self) -> Result<()> {
-        let mut buf = String::new();
-        // Set up logging span
-        let _entry = self.span.enter();
-
-        loop {
-            // Get the file metadata to get size
-            let new_metadata = self.reader.get_ref().metadata()?;
-            if new_metadata.len() <= self.metadata.len() {
-                debug!("file not changed, skipping");
-                continue;
-            }
-
-            // Block on reading a line
-            let n = self.reader.read_line(&mut buf)?;
-            debug!("got line");
-
-            if n == 0 {
-                continue;
-            }
-
-            info!("got new line");
-
-            // Construct the log message
-            let msg = LogMessage {
-                raw_line: buf.clone(),
-            };
-
-            // Send the line to the receiving channel
-            debug!("sending message");
-            self.sender.send(msg)?;
-
-            self.metadata = new_metadata;
-            buf.clear();
-        }
-
-        Ok(())
-    }
-}
 
 #[derive(Debug, StructOpt)]
 #[structopt(name = "tailers", version = "0.1.0")]
@@ -112,17 +33,32 @@ fn main() -> Result<()> {
     let span = span!(Level::TRACE, "adding-files");
     let enter = span.enter();
 
+    let mut watcher = raw_watcher(tx)?;
     for fname in opts.files {
         info!(%fname, "tailing log file");
-        let mut tailer = LogFileTailer::new(fname, &tx)?;
-        let _handle = tailer.run().expect("starting tailing thread");
+        watcher.watch(fname, RecursiveMode::NonRecursive)?;
     }
     drop(enter);
 
     info!("waiting for messages");
-
-    for msg in rx.iter() {
-        println!("{:?}", msg);
+    loop {
+        match rx.recv() {
+            Ok(RawEvent {
+                path: Some(path),
+                op: Ok(op),
+                cookie: _cookie,
+            }) => {
+                println!("got raw event {:?}, {:?}", path, op);
+            }
+            Ok(event) => {
+                eprintln!("got broken event: {:?}", event);
+                break;
+            }
+            Err(err) => {
+                eprintln!("error: {:?}", err);
+                break;
+            }
+        }
     }
 
     Ok(())
